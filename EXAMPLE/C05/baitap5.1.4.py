d2357ef23afc902f2d596c694cbdbec573a24121
minh phuong0075 <ltmphuong.dhkl16a1hn@sv.uneti.edu.vn>
2022-11-11 07:38:48 +0700

    Vừa code bài tập 5.1.4

---
+++ EXAMPLE/C05/baitap5.1.4.py
@@ -0,0 +1,9 @@
+#tính diện tích tam giác biết số đo 3 cạnh a, b, c theo công thức Hero
+import math     #import thư viện toán học để sử dụng hàm tính căn 2,sqrt
+print("Nhập số đo 3 cạnh của tam giác ABC")
+a = eval(input(' số đo cạnh a = '))
+b = eval(input(' số đo cạnh b = '))
+c = eval(input(' số đo cạnh c = '))
+p = (a+b+c)/2
+S = math.sqrt(p*(p-a)*(p-b)*(p-c))
+print("Diện tích tam giác ABC đã cho là: ", S)
