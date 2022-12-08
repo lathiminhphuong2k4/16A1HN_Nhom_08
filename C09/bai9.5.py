6a6232bf2240c39a8330c237a787bca0a3f4808a
Lã Thị Minh Phương <117744431+lathiminhphuong2k4@users.noreply.github.com>
2022-12-08 23:47:43 +0700

    Delete C09 directory

--- C09/bai9.5.py
+++
@@ -1,8 +0,0 @@
-#xây dựng phương thức tính A(n,x)
-import math
-def tinh_A(n,x):
-    A=math.pow(math.pow(x,2)+x+1,n)+math.pow(math.pow(x,2)-x+1,n)
-    return A
-n=int(input("Nhập n: "))
-x=int(input("Nhập x: "))
-print("A =",tinh_A(n,x))
